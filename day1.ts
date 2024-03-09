5db67fab158be492d38515d51b33312bcf3cbb48
igahmed <109594307+igahmed@users.noreply.github.com>
2024-03-09 15:54:00 +0500

    day1.ts
    solutions of day1 problem from the 100 days code challenge.

---
+++ day1.ts
@@ -0,0 +1,16 @@
+// DAY1
+console.log("Hello World!");
+
+//Q2
+let personName: string = "Shamveel";
+console.log(`Hello ${personName} would like to learn Typescript?`);
+
+//Q3
+//Storing person name in variable
+let personName: string = "Shamveel";
+//printing person name in Upper Case.
+console.log(`Name in upper case: ${personName.toUpperCase()}`);
+//printing person name in lower Case.
+console.log(`Name in lower case: ${personName.toLowerCase()}`);
+//printing person name in title Case.
+console.log(`Name in title case ${personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase()}`);
